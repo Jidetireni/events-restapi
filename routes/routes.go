71a68affde5a5e129244987117da8b0fac83915b
jidetireni <jtirenipraise@gmail.com>
2024-12-11 17:01:39 +0100

    add user handlers

--- routes/routes.go
+++ routes/routes.go
@@ -7,5 +7,7 @@ func RegisterRoutes(server *gin.Engine) {
 	server.POST("/events", createEvent)
 	server.GET("/events/:id", getEvent)
 	server.PUT("/events/:id", updateEvent)
-	server.DELETE("events/:id", deleteEvent)
+	server.DELETE("/events/:id", deleteEvent)
+	server.POST("/signup", signup)
+	server.POST("/login", login)
 }
