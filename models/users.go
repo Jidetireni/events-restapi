2a85661c2eea4ddcf8189efb4fb650b4b29c5347
jidetireni <jtirenipraise@gmail.com>
2024-12-11 17:01:02 +0100

    added user logic

---
+++ models/users.go
@@ -0,0 +1,57 @@
+package models
+
+import (
+	"errors"
+
+	"gitub.com/Jidetireni/events-restapi/db"
+	"gitub.com/Jidetireni/events-restapi/utils"
+)
+
+type User struct {
+	ID       int64
+	Email    string `binding:"required"`
+	Password string `binding:"required"`
+}
+
+func (u User) Save() error {
+	query := "INSERT INTO users(email, password) VALUES(?, ?)"
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	hashedPassword, err := utils.HashPassword(u.Password)
+	if err != nil {
+		return err
+	}
+
+	result, err := stmt.Exec(u.Email, hashedPassword)
+	if err != nil {
+		return err
+	}
+
+	userid, err := result.LastInsertId()
+	u.ID = userid
+	return err
+}
+
+func (u User) ValidateCred() error {
+	query := "SELECT password FROM users WHERE email = ?"
+	row := db.DB.QueryRow(query, u.Email)
+
+	var retrievedPassword string
+	err := row.Scan(&retrievedPassword)
+
+	if err != nil {
+		return err
+	}
+
+	passwordIsvalid := utils.ComparePassword(u.Password, retrievedPassword)
+
+	if !passwordIsvalid {
+		return errors.New("invalid credentials")
+	}
+	return nil
+}
