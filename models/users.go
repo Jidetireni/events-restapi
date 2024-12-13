4c0f85612ebb5e5e850cc57f0719915c63e02548
jidetireni <jtirenipraise@gmail.com>
2024-12-13 21:15:02 +0100

    add pionters to the user methods too

--- models/users.go
+++ models/users.go
@@ -13,7 +13,7 @@ type User struct {
 	Password string `binding:"required"`
 }
 
-func (u User) Save() error {
+func (u *User) Save() error {
 	query := "INSERT INTO users(email, password) VALUES(?, ?)"
 	stmt, err := db.DB.Prepare(query)
 	if err != nil {
@@ -37,7 +37,7 @@ func (u User) Save() error {
 	return err
 }
 
-func (u User) ValidateCred() error {
+func (u *User) ValidateCred() error {
 	query := "SELECT id, password FROM users WHERE email = ?"
 	row := db.DB.QueryRow(query, u.Email)
 
