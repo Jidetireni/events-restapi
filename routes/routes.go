ccba77e926ef60461092889ef5d68d3dc7efe396
jidetireni <jtirenipraise@gmail.com>
2024-12-14 22:11:40 +0100

    add the middleware to protect some routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,13 +1,20 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"gitub.com/Jidetireni/events-restapi/middleware"
+)
 
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
-	server.POST("/events", createEvent)
 	server.GET("/events/:id", getEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+
+	auth := server.Group("/")
+	auth.Use(middleware.Auth)
+	auth.POST("/events", createEvent)
+	auth.PUT("/events/:id", updateEvent)
+	auth.DELETE("/events/:id", deleteEvent)
+
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
